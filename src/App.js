46e18057b19bac0e893321f1b7bc380ce2741570
katherineRamirez <oeillet8_5@hotmail.com>
2018-03-22 12:45:32 -0300

    mapa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ContainerMap from './components/ContainerMap/Map';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ContainerMap> </ContainerMap>
+      </div>
+    );
+  }
+}
+
+export default App;
